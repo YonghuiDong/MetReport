0701298300a7b4b57d373889f55d7d0ccba89782
Dong <yonghui.dong@gmail.com>
2022-12-25 21:08:12 +0200

    Create fct_showPCA.R

---
+++ R/fct_showPCA.R
@@ -0,0 +1,81 @@
+#' @title Show PCA score plot
+#' @description perform PCA and show the score plot
+#' @author Yonghui Dong
+#' @param dat sample ion intensity matrix, row sample, column feature.
+#' @param Group sample group information
+#' @param inx PCA X axis, default is PC1
+#' @param iny PCA Y axis, defult is PC2
+#' @param showFrame what kind of frame should be added? Default is none
+#' @param interactive should the plot be interactive? default is FALSE
+#' @importFrom stats prcomp
+#' @importFrom grDevices chull
+#' @importFrom ggplot2 ensym
+#' @importFrom dplyr %>%
+#' @importFrom plotly ggplotly
+#' @export
+#' @noRd
+#' @examples
+#' dat <-  iris[1:4]
+#' Group <- iris$Species
+#' showPCA(dat, Group, showFrame = "polygon", interactive = T)
+
+showPCA <- function(dat,
+                    Group,
+                    inx = 1,
+                    iny = 2,
+                    showFrame = "none",
+                    interactive = FALSE) {
+
+  #(1) check input
+  Group <- as.factor(Group)
+  if(is.null(Group)){stop("Please include group information")}
+  if(length(levels(Group)) <= 1){stop("At least two sample groups should be included")}
+  if(length(Group) != nrow(dat)){stop("Missing group informaiton detected")}
+
+  #(2) perform PCA
+  df_pca <- prcomp(dat, center = FALSE, scale. = FALSE)
+  df_pcs <- data.frame(df_pca$x, Group = Group)
+  percentage <-round(df_pca$sdev^2 / sum(df_pca$sdev^2) * 100, 2)
+  percentage <-paste(colnames(df_pcs),"(", paste(as.character(percentage), "%", ")", sep = ""))
+
+  #(2) plot
+  nms <- names(df_pcs)
+  x <- nms[inx]
+  y <- nms[iny]
+
+  Sample_Name <- rownames(df_pcs) # rename
+  p <- ggplot2::ggplot(df_pcs, aes(x = !!ensym(x), y = !!ensym(y), color = Group, label = Sample_Name)) +
+    ggplot2::xlab(percentage[inx]) +
+    ggplot2::ylab(percentage[iny]) +
+    ggplot2::theme_bw()
+
+  if(showFrame == "none"){
+    p2 <- p +
+      ggplot2::geom_point(size = 3)
+  }
+
+  if(showFrame == "polygon"){
+    hull_group <- df_pcs %>%
+      dplyr::mutate(Sample_Name = Sample_Name) %>%
+      dplyr::group_by(Group) %>%
+      dplyr::slice(chull(!!ensym(x), !!ensym(y)))
+    p2 <- ggplot2::ggplot() +
+      ggplot2::geom_polygon(data = hull_group, mapping =  aes(x = !!ensym(x), y = !!ensym(y), fill = Group, group = Group), alpha = 0.2) +
+      ggplot2::geom_point(data = df_pcs, mapping = aes(x = !!ensym(x), y = !!ensym(y), color = Group, label = Sample_Name), size = 3) +
+      ggplot2::xlab(percentage[inx]) +
+      ggplot2::ylab(percentage[iny]) +
+      ggplot2::theme_bw()
+  }
+
+  if(showFrame == "norm"){
+    p2 <- p +
+      ggplot2::stat_ellipse(aes(fill = Group), geom = "polygon", level = 0.95, alpha = 0.1, show.legend = F) +
+      ggplot2::geom_point(size = 3)
+  }
+
+  if(interactive){
+    p2 <- ggplotly(p2, tooltip = "label")
+  }
+
+  return(p2)
+}

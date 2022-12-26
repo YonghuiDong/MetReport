bdfff808c12db14ada1e993f9aa6c2ae0f9f3165
Dong <yonghui.dong@gmail.com>
2022-12-26 15:58:24 +0200

    Create fct_showVolcano.R

---
+++ R/fct_showVolcano.R
@@ -0,0 +1,77 @@
+#' @title View volcano plot
+#' @description View volcano plot.
+#' @author Yonghui Dong
+#' @param result result from doStat() function
+#' @param compare_group which groups you want to compare, i.e. c("WT", "Treat1"), only two groups are allowed
+#' @param FC select fold change values, default = 2
+#' @param pValue select p value, default = 0.05
+#' @importFrom ggplot2 aes
+#' @importFrom plotly ggplotly
+#' @noRd
+#' @export
+#'@examples
+#'\dontrun{
+#'showVolcano(result, compare_group = c("WT", "JA"))
+#'}
+
+showVolcano <- function(result, compare_group, FC = 2, pValue = 0.05, interactive = TRUE) {
+
+  #(1)check input
+  if(!is.numeric(FC)){stop("invalid calss of fold change: not numeric")}
+  if(!is.numeric(pValue)){stop("invalid calss of p value: not numeric")}
+  if(pValue > 1){stop("p value should between 0 and 1")}
+  if(FC < 0){stop("FC value cannot be negative value ")}
+  ## suppress "no visible binding for global variable" notes in ggplot2
+  fold_change <- p_value <- significant <- NULL
+
+  #(2) get index
+  F_id <- paste("Fold_", compare_group[1], "_vs_", compare_group[2], sep = "")
+  P_id1 <- paste("AdjPvalue_", compare_group[1], "_vs_", compare_group[2], sep = "")
+  P_id2 <- paste("AdjPvalue_", compare_group[2], "_vs_", compare_group[1], sep = "")
+
+  #(3) plot
+  ## select which p value colname exist.
+  if ((P_id1 %in% colnames(result)) == TRUE) {
+    P_id = P_id1
+    } else if ((P_id2 %in% colnames(result)) == TRUE) {
+      P_id = P_id2
+      } else {stop("The selected groups do not exist")}
+
+  ## prepare data frame for plot
+  F_iden <- result[, F_id]
+  P_iden <- result[, P_id]
+  new_iden <- cbind.data.frame(fold_change = log2(F_iden), p_value = -log10(P_iden))
+  colnames(new_iden) <- c("fold_change", "p_value") # to make sure colnames changed
+
+  ## sometimes there are inf, NAN values in FC and p-value, so i replace these values with max(FC) and 0 for FC and p, respectively
+  if(length(new_iden[!is.finite(new_iden$fold_change), ]$fold_change) > 0)
+    {new_iden[!is.finite(new_iden$fold_change), ]$fold_change <- max(abs(new_iden[is.finite(new_iden$fold_change),]$fold_change))}
+  if(length(new_iden[!is.finite(new_iden$p_value), ]$p_value) > 0)
+    {new_iden[!is.finite(new_iden$p_value), ]$p_value <- 0}
+
+  ## add color information (significance)
+  new_iden$significant = "Not Significant"
+  con1 <- new_iden[(new_iden$fold_change >= log2(FC) & new_iden$p_value >= -log10(pValue)),]
+  if(dim(con1)[1] > 0) {
+    new_iden[(new_iden$fold_change >= log2(FC) & new_iden$p_value >= -log10(pValue)),]$significant = "Significantly Up"
+    }
+
+  con2 <- new_iden[(new_iden$fold_change < -log2(FC) & new_iden$p_value >= -log10(pValue)),]
+  if(dim(con2)[1] > 0) {
+    new_iden[(new_iden$fold_change < -log2(FC) & new_iden$p_value >= -log10(pValue)),]$significant = "Significantly Down"
+    }
+
+  ##plot
+  VP <- ggplot2::ggplot(new_iden, aes(x = fold_change, y = p_value, color = significant, text = rownames(result))) +
+    ggplot2::geom_point() +
+    ggplot2::scale_color_manual(name = "Type", values = c("Grey", "steelblue1", "Tomato")) +
+    ggplot2::xlab("Log2(FC)") +
+    ggplot2::ylab("-Log10(p-Value)") +
+    ggplot2::ggtitle(paste("Volcano plot for sample groups:", compare_group[1], "vs", compare_group[2], sep = " ")) +
+    ggplot2::theme_bw() +
+    ggplot2::geom_vline(xintercept = c(-log2(FC), log2(FC)), col = "grey", linetype = "longdash") +
+    ggplot2::geom_hline(yintercept = -log10(pValue), col = "grey", linetype = "longdash")
+  VP2 <- plotly::ggplotly(VP, tooltip = c("text"))
+  if(!isTRUE(interactive)){VP2 <- VP}
+  return(VP2)
+}

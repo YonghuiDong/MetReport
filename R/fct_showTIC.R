a74f0c30c3058ce8bcef9cdc3cfb3bcb82571845
Dong <yonghui.dong@gmail.com>
2022-12-29 23:00:25 +0200

    Update fct_showTIC.R

--- R/fct_showTIC.R
+++ R/fct_showTIC.R
@@ -1,26 +1,24 @@
 #' @title Plot TIC
-#' @description Plot TIC as bar plot.
+#' @description Plot peak area as box plot.
 #' @param df dataframe, row is feature, and column is sample
 #' @param Group sample group information
-#' @importFrom ggplot2 aes element_text
-#' @return The return value, if any, from executing the function.
+#' @importFrom ggplot2 aes element_blank
+#' @importFrom tidyr pivot_longer
+#' @return boxplot
 #' @export
 #' @noRd
 
 showTIC <- function(df, Group = NULL){
   #(1) format data
-  tem <- df[, !(names(df) %in% c("ID"))]
-  tem <- as.data.frame(colSums(tem), col.name = "TIC")
-  tem$Sample <- rownames(tem)
-  tem$Groups <- Group
-  colnames(tem)[1] <- "TIC"
-  colnames(tem)[3] <- "Groups"
+  tem <- t(df[, !(names(df) %in% c("ID"))])
+  tem <- cbind.data.frame(Group = Group, Sample = rownames(tem), tem)
+  colnames(tem)[1] <- "Group"
+  tem <- tidyr::pivot_longer(tem, cols = !c(Group, Sample), names_to = "name", values_to = "Area")
+
   #(2) ploy
-  ggplot2::ggplot(tem, aes(x = Sample, y = TIC, fill = Groups)) +
-    ggplot2::geom_bar(stat = "identity",
-                      color="black",
-                      size=0.1,
-                      alpha = 0.7) +
+  ggplot2::ggplot(tem, aes(x = Sample, y = log10(Area), fill = Group)) +
+    ggplot2::geom_boxplot(alpha = 0.7) +
+    ggplot2::ylab("Peak Area") +
     ggplot2::theme_bw() +
-    ggplot2::theme(axis.text.x = element_text(angle = 90, hjust = 1))
-  }
+    ggplot2::theme(axis.text.x = element_blank())
+}

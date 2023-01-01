fef05bf1dd67cdebb016140e257669573d9b2887
Dong <yonghui.dong@gmail.com>
2023-01-01 21:14:19 +0200

    Update mod_01_home.R

--- R/mod_01_home.R
+++ R/mod_01_home.R
@@ -11,7 +11,7 @@ mod_01_home_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-      column(width = 10, includeMarkdown("inst/app/www/landing.md"))
+      column(width = 10, includeMarkdown(app_sys("app/www/landing.md")))
       )
     )
 }

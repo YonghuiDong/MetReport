e1b95bc34c162dd29144f65431e5b0604a3c063b
Dong <yonghui.dong@gmail.com>
2023-01-05 22:36:15 +0200

    #8

--- R/app_ui.R
+++ R/app_ui.R
@@ -96,5 +96,7 @@ golem_add_external_resources <- function(){
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
   )
+  # add css file
+  tags$link(rel = "stylesheet", type = "text/css", href = "www/custom.css")
 }
 

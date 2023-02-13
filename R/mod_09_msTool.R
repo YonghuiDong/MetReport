4f6b1b8e27debdf25ad0337482332aef023cd457
Dong <yonghui.dong@gmail.com>
2023-02-13 10:33:13 +0200

    #27

--- R/mod_09_msTool.R
+++ R/mod_09_msTool.R
@@ -20,12 +20,17 @@ mod_09_msTool_ui <- function(id){
                solidHeader = FALSE,
                collapsible = TRUE,
                collapsed = FALSE,
-               closable = FALSE
+               closable = FALSE,
+               p("1. This widget is designed to calculate m/z values, and identidy mass based on m/z values."),
+               p("2. You have the option to either enter a single value or upload a list of values. If uploading
+                 a list, please ensure that the correct required names are used."),
+               p("3. The result can be downloaded in csv format."),
+               p("4. Note that when using this module for the first time, it will take a bit longer to load the database.
+                 However, subsequent calculations will be significantly faster.")
                )
              ),
 
       #2. Data Input Panel =====================================================
-
       ## 1. Calculate m/z values -----------------------------------------------
       column(width = 4,
              box(
@@ -45,11 +50,8 @@ mod_09_msTool_ui <- function(id){
                fileInput(inputId = ns("formula2"),
                          label = "1. (alternatively) Upload a list of formulas:",
                          multiple = FALSE,
-                         placeholder = "should contain a column called Formula",
-                         accept = c(".csv",
-                                    ".xls",
-                                    ".xlsx"
-                                    )
+                         placeholder = "should contain a column named Formula",
+                         accept = c(".csv", ".xls", ".xlsx")
                          ),
                numericInput(inputId = ns("nCharge"),
                             label = "Input number of charge",
@@ -63,14 +65,16 @@ mod_09_msTool_ui <- function(id){
                             label = "Ion mode?",
                             choices = list("Positive" = 1, "Negative" = -1),
                             selected = NA
-                            )),
+                            )
+                      ),
                column(width = 7, uiOutput(outputId = ns("adducts"))),
                column(width = 12,
                       actionButton(inputId = ns("getMZ"),
                             label = "Calculate",
                             icon = icon("paper-plane"),
                             style = "color: #fff; background-color: #4daf4a; border-color: #4daf4a"
-                            ))
+                            )
+                      )
                )
              ),
 
@@ -94,10 +98,7 @@ mod_09_msTool_ui <- function(id){
                          label = "1. (alternatively) Upload a list of mz values:",
                          multiple = FALSE,
                          placeholder = "should contain a column named mz",
-                         accept = c(".csv",
-                                    ".xls",
-                                    ".xlsx"
-                                    )
+                         accept = c(".csv", ".xls", ".xlsx")
                          ),
                numericInput(inputId = ns("ppmTolerance"),
                             label = "Input mass accuracy tolerance (ppm)",
@@ -138,10 +139,7 @@ mod_09_msTool_ui <- function(id){
                          label = "1. (alternatively) Upload a list of noise mz values:",
                          multiple = FALSE,
                          placeholder = "should contain a column named mz",
-                         accept = c(".csv",
-                                    ".xls",
-                                    ".xlsx"
-                                    )
+                         accept = c(".csv", ".xls", ".xlsx")
                          ),
                numericInput(inputId = ns("ppmTolerance2"),
                             label = "Input mass accuracy tolerance (ppm)",
@@ -173,6 +171,9 @@ mod_09_msTool_ui <- function(id){
                collapsible = TRUE,
                collapsed = FALSE,
                closable = FALSE,
+               downloadButton(outputId = ns("downloadmz")),
+               br(),
+               br(),
                DT::dataTableOutput(ns("mzOutput"))
                )
              ),
@@ -186,6 +187,9 @@ mod_09_msTool_ui <- function(id){
                collapsible = TRUE,
                collapsed = FALSE,
                closable = FALSE,
+               downloadButton(outputId = ns("downloadDBOutput")),
+               br(),
+               br(),
                DT::dataTableOutput(ns("dbOutput"))
                )
              ),
@@ -200,6 +204,9 @@ mod_09_msTool_ui <- function(id){
                collapsible = TRUE,
                collapsed = FALSE,
                closable = FALSE,
+               downloadButton(outputId = ns("downloadNoiseOutput")),
+               br(),
+               br(),
                DT::dataTableOutput(ns("noiseOutput"))
                )
              )
@@ -213,12 +220,11 @@ mod_09_msTool_ui <- function(id){
 mod_09_msTool_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-
     ############################################################################
-    # 1. Calculate m/z values
+    # Calculate m/z values======================================================
     ############################################################################
 
-    #(1) Update adducts based on ion mode
+    #(1) Update adducts based on ion mode=======================================
     ## (1.1) Get ionization mode -----------------------------------------------
     ionMode1 <- reactive({
       if(is.null(input$ionMode1)) {return(NULL)}
@@ -229,7 +235,7 @@ mod_09_msTool_server <- function(id){
       if(is.null(ionMode1())){
         return(NULL)
       } else if((as.numeric(ionMode1()) == -1)){
-        return(c("H", "CH2O2", "CH3COOH", "HPO3", "HSO3"))
+        return(c("H", "CH2O2", "CH3COOH"))
       } else{
         return(c("H", "Na", "K", "H30", "NH4"))
       }
@@ -242,14 +248,14 @@ mod_09_msTool_server <- function(id){
                   )
       })
 
+    ##(2) Get and show result ==================================================
     observeEvent(input$getMZ, {
-      ## (1) Load Data =========================================================
+      ## (1) Load Data ---------------------------------------------------------
       ###(1.1) get formula
       formula1 <- reactive({
         if(is.null(input$formula1)) {return(NULL)}
         return(input$formula1)
         })
-
       formula2 <- reactive({
         inFile <- input$formula2
         if(is.null(inFile)){return(NULL)}
@@ -262,7 +268,6 @@ mod_09_msTool_server <- function(id){
                      )
         return(df)
         })
-
       if(is.null(formula2()$Formula)) {
         formula = formula1()
         } else {
@@ -275,12 +280,11 @@ mod_09_msTool_server <- function(id){
         return(input$nCharge)
         })
 
-      ## (2) Result ============================================================
-      output$mzOutput <- DT::renderDataTable({
+      ###(1.3) Get result ------------------------------------------------------
+      mzValue <- reactive({
         shiny::validate(need(formula != "", message = "No formula found"))
         shiny::validate(need(nCharge() > 0, message = "number of charge should be positive value"))
         shiny::validate(need(!is.null(ionMode1()), message = "Please select ion mode"))
-
         shiny::withProgress(
           message = "It takes time to load the database when you use this module for the first time ",
           detail = "Be patient...",
@@ -293,7 +297,13 @@ mod_09_msTool_server <- function(id){
                              mode = mode
                              )
             })
-        DT::datatable(mzValue,
+        return(mzValue)
+        })
+
+      ###(1.4) Show Result -----------------------------------------------------
+      output$mzOutput <- DT::renderDataTable({
+        shiny::req(mzValue())
+        DT::datatable(mzValue(),
                       rownames = FALSE,
                       options = list(scrollX = TRUE,
                                      deferRender = TRUE,
@@ -302,21 +312,26 @@ mod_09_msTool_server <- function(id){
                                      )
                       )
         })
+      ###(1.5) Download Result -------------------------------------------------
+      output$downloadmz <- downloadHandler(
+        filename = "calculatedMZ.csv",
+        content = function(file){
+          write.csv(mzValue(), file, row.names = FALSE)
+          }
+        )
       })
 
-    ##########################################
-    # 2. Search m/z values in DB
-    ##########################################
+    ############################################################################
+    # Search m/z values in DB ==================================================
+    ############################################################################
 
-    # 1 Search m/z==============================================================
+    #(1)Search m/z =============================================================
     observeEvent(input$searchMZ, {
-      ## (1) Load Data ---------------------------------------------------------
-      ###(1.1) get formula -----------------------------------------------------
+      ##(1.1) get formula ------------------------------------------------------
       mz1 <- reactive({
         if(is.null(input$mz1)) {return(NULL)}
         return(as.numeric(input$mz1))
         })
-
       mz2 <- reactive({
         inFile <- input$mz2
         if(is.null(inFile)){return(NULL)}
@@ -329,31 +344,28 @@ mod_09_msTool_server <- function(id){
                      )
         return(df)
         })
-
       if(is.null(mz2()$mz)) {
         mzList = mz1()
         } else {
           mzList = as.numeric(mz2()$mz)
           }
 
-      ###(1.2) Get mass accuracy tolerance--------------------------------------
+      ##(1.2) Get mass accuracy tolerance --------------------------------------
       ppmTolerance <- reactive({
         return(input$ppmTolerance)
         })
 
-      ###(1.3) Get ionization mode----------------------------------------------
+      ##(1.3) Get ionization mode ----------------------------------------------
       ionMode2 <- reactive({
         if(is.null(input$ionMode2)) {return(NULL)}
         return(input$ionMode2)
         })
 
-      ##(2) Result =============================================================
-
-      output$dbOutput <- DT::renderDataTable({
+      ##(1.4) Get Result -------------------------------------------------------
+      idList <- reactive({
         shiny::validate(need(mzList != "", message = "No mz value found"))
         shiny::validate(need(ppmTolerance() > 0, message = "Mass tolerance (ppm) should be positive value"))
         shiny::validate(need(!is.null(ionMode2()), message = "Please select ion mode"))
-
         shiny::withProgress(
           message = "Searching in HMDB database ",
           detail = "Be patient...",
@@ -365,7 +377,13 @@ mod_09_msTool_server <- function(id){
                                   useDB = "HMDB")
           })
         if(is.null(idList)){idList = data.frame(Result = "Not Found")}
-        DT::datatable(idList,
+        return(idList)
+      })
+
+      #(1.5) Show Result -------------------------------------------------------
+      output$dbOutput <- DT::renderDataTable({
+        shiny::req(idList())
+        DT::datatable(idList(),
                       rownames = FALSE,
                       options = list(scrollX = TRUE,
                                      deferRender = TRUE,
@@ -374,22 +392,28 @@ mod_09_msTool_server <- function(id){
                                      )
                       )
         })
+
+      ##(1.6) Download Result-------------------------------------------------
+      output$downloadDBOutput <- downloadHandler(
+        filename = "Identification_HMDB.csv",
+        content = function(file){
+          write.csv(idList(), file, row.names = FALSE)
+          }
+        )
       })
 
 
-    ##########################################
+    ############################################################################
     # 3. Identify noise m/z peaks
-    ##########################################
+    ############################################################################
 
     #(1) Search m/z=============================================================
     observeEvent(input$searchNoise, {
-      ##(1) Load Data ----------------------------------------------------------
       ###(1.1) get formula -----------------------------------------------------
       noise1 <- reactive({
         if(is.null(input$noise1)) {return(NULL)}
         return(as.numeric(input$noise1))
         })
-
       noise2 <- reactive({
         inFile <- input$noise2
         if(is.null(inFile)){return(NULL)}
@@ -402,43 +426,46 @@ mod_09_msTool_server <- function(id){
                      )
         return(df)
         })
-
       if(is.null(noise2()$mz)) {
         noiseList = noise1()
         } else {
           noiseList = as.numeric(noise2()$mz)
           }
 
-      ###(1.2) Get mass accuracy tolerance -------------------------------------
+      ##(1.2) Get mass accuracy tolerance --------------------------------------
       ppmTolerance2 <- reactive({
         return(input$ppmTolerance2)
         })
 
-      ###(1.3) Get ionization mode ---------------------------------------------
+      ##(1.3) Get ionization mode ----------------------------------------------
       ionMode3 <- reactive({
         if(is.null(input$ionMode3)) {return(NULL)}
         return(input$ionMode3)
         })
 
-
-      ## (2) Result ============================================================
-      output$noiseOutput <- DT::renderDataTable({
+      #(2) Result ==============================================================
+      ##(1) Get Result----------------------------------------------------------
+      getNoiseList <- reactive({
         shiny::validate(need(noiseList != "", message = "No mz value found"))
         shiny::validate(need(ppmTolerance2() > 0, message = "Mass tolerance (ppm) should be positive value"))
         shiny::validate(need(!is.null(ionMode3()), message = "Please select ion mode"))
-
         shiny::withProgress(
           message = "Identifying contanminates",
           detail = "Be patient...",
           value = 0.4,
           {
-            noiseList <- MSbox::contam(mz = noiseList,
-                                       mode = ionMode3(),
-                                       ppm = ppmTolerance2())
+            getNoiseList <- MSbox::contam(mz = noiseList,
+                                          mode = ionMode3(),
+                                          ppm = ppmTolerance2())
           })
+        if(is.null(getNoiseList)){getNoiseList = data.frame(Result = "Not Found")}
+        return(getNoiseList)
+      })
 
-        if(is.null(noiseList)){noiseList = data.frame(Result = "Not Found")}
-        DT::datatable(noiseList,
+      ##(1.2) Show Result ------------------------------------------------------
+      output$noiseOutput <- DT::renderDataTable({
+        shiny::req(getNoiseList())
+        DT::datatable(getNoiseList(),
                       rownames = FALSE,
                       options = list(scrollX = TRUE,
                                      deferRender = TRUE,
@@ -447,6 +474,13 @@ mod_09_msTool_server <- function(id){
                                      )
                       )
         })
+      ##(1.3) Download Result-------------------------------------------------
+      output$downloadNoiseOutput <- downloadHandler(
+        filename = "noisePeak.csv",
+        content = function(file){
+          write.csv(getNoiseList(), file, row.names = FALSE)
+          }
+        )
       })
     })
   }

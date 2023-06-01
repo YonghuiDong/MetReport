12c2dd42aec4b2cf7796f873f8f1278171a49e8e
Dong <yonghui.dong@gmail.com>
2023-06-01 15:45:34 +0300

    Update mod_01_home.R

--- R/mod_01_home.R
+++ R/mod_01_home.R
@@ -12,9 +12,8 @@ mod_01_home_ui <- function(id){
   tagList(
     fluidRow(
       column(width = 10, includeMarkdown(app_sys("app/www/landing.md")))
-    )
-  )
-}
+
+))}
 
 #' 01_home Server Functions
 #'
@@ -22,8 +21,8 @@ mod_01_home_ui <- function(id){
 mod_01_home_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
-  })
-}
+
+})}
 
 ## To be copied in the UI
 # mod_01_home_ui("01_home_1")
